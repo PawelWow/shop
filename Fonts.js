095e18d2da973090b61c13fd4090ee5a536efe8b
pawel <pawel@webperfect.pl>
2020-08-18 20:54:56 +0200

    fonts

--- Fonts.js
+++ Fonts.js
@@ -5,7 +5,7 @@ export const FONT_OPEN_SANS_BOLD = 'open-sans-bold';
 
 export const fetchFonts = () => {
     return Font.loadAsync({
-        'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
-        'open-sans': require('./assets/fonts/OpenSans-Regular.ttf')
+        [FONT_OPEN_SANS_BOLD]: require('./assets/fonts/OpenSans-Bold.ttf'),
+        [FONT_OPEN_SANS_REGULAR]: require('./assets/fonts/OpenSans-Regular.ttf')
     });
 };
