b44acce3f629596c6b8face4caabb775dd7260dc
pawel <pawel@webperfect.pl>
2020-08-19 13:48:28 +0200

    clearing cart after creating order

--- components/screens/shop/OrdersScreen.js
+++ components/screens/shop/OrdersScreen.js
@@ -11,7 +11,7 @@ const OrdersScreen = props => {
         <FlatList
             data={orders}
             keyExtractor={ item => item.id}
-            renderItem={itemData => <Text>{itemData.itemTotalAmount}</Text>}/>
+            renderItem={itemData => <Text>{itemData.item.totalAmount}</Text>}/>
     );
 };
 
--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -1,4 +1,5 @@
 import { ADD_TO_CART, REMOVE_FROM_CART } from '../actions/cart';
+import { ADD_ORDER } from '../actions/orders';
 import CartItem from '../../models/cart-item';
 
 const initialState = {
@@ -57,6 +58,8 @@ export default (state = initialState, action) => {
                     totalAmount: state.totalAmount - currentItem.sum
                 }
             }
+        case ADD_ORDER:
+            return initialState;
 
     }
 
