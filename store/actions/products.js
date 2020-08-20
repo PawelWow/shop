33b17b27e0fac36f762b803df17e6761d9f2e370
pawel <pawel@webperfect.pl>
2020-08-20 11:38:33 +0200

    updating items on the server

--- store/actions/products.js
+++ store/actions/products.js
@@ -72,12 +72,28 @@ export const createProduct = (title, description, imageUrl, price) => {
 };
 
 export const updateProduct = (id, title, description, imageUrl) => {
-    return { type: UPDATE_PRODUCT, 
-        pid: id,
-        productData: {
-            title,
-            description,
-            imageUrl
-        }
+    return async dispatch => {
+
+        await fetch(`https://shop-rn-guide.firebaseio.com/products/${id}.json`, { 
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title,
+                description,
+                imageUrl
+            })
+         });
+
+        dispatch({ 
+            type: UPDATE_PRODUCT, 
+            pid: id,
+            productData: {
+                title,
+                description,
+                imageUrl
+            }
+        });
     };
 };
